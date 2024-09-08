552b3564143056b8e3f9db2e2c61f66717a6a289
Rithas K <rithask@icloud.com>
2024-09-08 21:57:18 +0530

    feat: add login route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
@@ -29,6 +30,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
