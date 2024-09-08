01347ba8272e8cf8c43bdf8be83912f55823723d
Rithas K <rithask@icloud.com>
2024-09-08 19:48:44 +0530

    feat: add User model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
