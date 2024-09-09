2dc0a5be0b61120b22e291521c0aa1d87d593964
Rithas K <rithask@icloud.com>
2024-09-09 16:04:32 +0530

    fix: typo

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
-const JWT_SECRET = process.env.SECRET;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 module.exports = {
   MONGODB_URI,
