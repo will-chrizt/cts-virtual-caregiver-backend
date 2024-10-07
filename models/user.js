e8baa825dd416a93a519f9f5f61641890db10aa2
Amith P <107483173+amithpalissery@users.noreply.github.com>
2024-10-07 16:54:55 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  name: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
   email: {
     type: String,
     required: true,
